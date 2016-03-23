e90c8aac2641bac0b450d960a175e35e3fd3cdf1
Thomas Cokelaer <cokelaer@gmail.com>
2016-03-23 23:41:55 +0100

    update setup to find packages

--- setup.py
+++ setup.py
@@ -1,12 +1,103 @@
-#from distutils.core import setup
-from setuptools import setup
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$"
+import sys
+import os
+from setuptools import setup, find_packages
+import glob
 
-setup(name='FragmentAnalyser',
-      version='0.1',
-      description='Fragment Analyser utilities',
-      author='Thomas Cokelaer',
-      author_email='thomas.cokelaer@pasteur.fr',
-      url='https://github.com/cokelaer/FragmentAnalyser',
-      scripts=['fragment_analyser.py']
-     )
+
+_MAJOR               = 0
+_MINOR               = 2
+_MICRO               = 0 
+version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release              = '%d.%d' % (_MAJOR, _MINOR)
+
+
+metainfo = {
+    'authors': {
+        'Cokelaer':('Thomas Cokelaer','thomas.cokelaer@pasteur.fr'),
+        },
+    'version': version,
+    'license' : 'BSD',
+    'download_url' : ['http://pypi.python.org/pypi/fragment_analyser'],
+    'url' : ['http://pypi.python.org/pypi/gdsctools'],
+    'description': 'tools to interpret output data from Fragment Analyser Machines' ,
+    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'keywords' : ['genome', 'DNA', 'Fragment'],
+    'classifiers' : [
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics']
+    }
+
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+from distutils.core import setup, Extension
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#
+
+# on rtd, pandas is 1.3.1 cannot use something better for now
+if on_rtd is True:  # only import and set the theme if we're building docs
+    install_requires = ['colormap', 'easydev']
+else:
+    install_requires = ['numpy', 'matplotlib>=1.4.3', 'pandas>=0.16.2', 'easydev>=0.9.5', ],
+
+
+setup(
+    name             = 'fragment_analyser',
+    version          = version,
+    maintainer       = metainfo['authors']['Cokelaer'][0],
+    maintainer_email = metainfo['authors']['Cokelaer'][1],
+    author           = metainfo['authors']['Cokelaer'][0],
+    author_email     = metainfo['authors']['Cokelaer'][1],
+    long_description = readme,
+    keywords         = metainfo['keywords'],
+    description = metainfo['description'],
+    license          = metainfo['license'],
+    platforms        = metainfo['platforms'],
+    url              = metainfo['url'],
+    download_url     = metainfo['download_url'],
+    classifiers      = metainfo['classifiers'],
+
+    # package installation
+    package_dir = {'share.data': 'share/data', '':'src'},
+    packages = ['fragment_analyser'],
+  
+
+    zip_safe=False,
+    #packages = find_packages(),
+    include_package_data = True,
+
+
+    # here below '': pattern means include that pattern in all packages
+    # so '' :['README.rst'] will include all README.rst recursively
+    package_data = {
+        'share.data' : ['*.txt', '*.csv', '*.tsv', '*.gz', 'README.rst'],
+        '' : ['README.rst'],
+        },
+
+    # comment the requirements otherwise RTD fails
+    # but we then need a requirements.txt file !
+    install_requires = install_requires,
+    entry_points = {
+        'console_scripts': [
+        'fragment_analyser=fragment_analyser.pipelines:main',]
+        },
+
+
+    )
+
 
